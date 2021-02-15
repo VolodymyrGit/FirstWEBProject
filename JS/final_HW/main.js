0d5eb85ed7959bf4c2c12af3e70a05cca8de58d1
Melnyk_Volodymyr <olgreen123@gmail.com>
2021-02-15 14:40:23 +0200

    In progress

---
+++ JS/final_HW/main.js
@@ -0,0 +1,35 @@
+let calculator = document.getElementById('calculator');
+calculator.classList.add('calculator');
+
+
+let buttons = document.getElementById('buttons');
+buttons.classList.add('buttons');
+
+
+let operation = document.getElementsByClassName('operation');
+
+
+let numbers = document.getElementById('numbers');
+numbers.classList.add('numbers');
+
+
+let num1 = document.getElementById('num1');
+num1.classList.add('num1', 'd-flex');
+
+
+let num2 = document.getElementById('num2');
+num2.classList.add('num2', 'd-flex');
+
+
+let res = document.getElementById('res');
+res.classList.add('res', 'd-flex');
+
+
+let calculationResult = document.getElementById('calculationResult');
+calculationResult.classList.add('calculationResult');
+
+// .container {
+//     width: 90%;
+//     height: 100%;
+//     margin: 0 auto;
+// }
